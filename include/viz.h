cc0771e5fc8aa007725a6790d0bc8800cca1e39f
Gabriel Moreira <43323783+gabmoreira@users.noreply.github.com>
2019-11-01 03:34:18 +0000

    Added mouse dragging feature

--- include/viz.h
+++ include/viz.h
@@ -5,6 +5,8 @@
 #ifndef VIZ_H
 #define VIZ_H
 
+#define NCLOUDS 10
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
@@ -12,39 +14,82 @@
 #include <glm/gtc/type_ptr.hpp>
 #include "shader.h"
 #include <mutex>
+#include <future>
 
 std::mutex g_display_mutex;
 
-const unsigned int SCR_WIDTH  = 1800;
-const unsigned int SCR_HEIGHT = 1400;
+//------------------------------------------------------------------------------
+// VERTEX AND ATTRIBUTE BUFFERS
+unsigned int VBO;
+unsigned int VAO;
+//------------------------------------------------------------------------------
 
+//------------------------------------------------------------------------------
+// SCREEN
+const unsigned int SCR_WIDTH  = 800;
+const unsigned int SCR_HEIGHT = 500;
+float ASPECT_RATIO = (float) SCR_WIDTH / (float) SCR_HEIGHT;
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// CAMERA'S COORDINATE SYSTEM AXIS INITIALIZATION
 typedef struct Camera {
   glm::vec3 pos;
   glm::vec3 front;
   glm::vec3 up;
+  float fov;
+  float nearField;
+  float farField;
 }Camera;
 
 Camera camera = {glm::vec3(0.0f, 0.0f, 35.0f),
                  glm::vec3(0.0f, 0.0f, -1.0f),
-                 glm::vec3(0.0f, 1.0f,  0.0f)};
+                 glm::vec3(0.0f, 1.0f,  0.0f),
+                 45.0f,
+                 0.1f,
+                 100.0f};
+//------------------------------------------------------------------------------
 
-bool firstMouse = true;
-float yaw       = -90.0f;
-float pitch     =  0.0f;
-float lastX     =  800.0f / 2.0;
-float lastY     =  600.0 / 2.0;
-float fov       =  45.0f;
+//------------------------------------------------------------------------------
+// MOUSE CONTROL GLOBAL VARIABLES
+bool firstMouse        = true;
+bool leftButtonPressed = false;
+float lastX            =  SCR_WIDTH / 2.0;
+float lastY            =  SCR_HEIGHT / 2.0;
+//------------------------------------------------------------------------------
 
-glm::mat4 model[3]   = {glm::mat4(1.0f), glm::mat4(1.0f), glm::mat4(1.0f)};
-glm::mat4 view       =  glm::mat4(1.0f);
-glm::mat4 projection =  glm::mat4(1.0f);
+//------------------------------------------------------------------------------
+// COORDINATE SYSTEM TRANSFORMATION MATRICES
+//------------------------------------------------------------------------------
+glm::mat4 model[NCLOUDS]  = {glm::mat4(1.0f),
+                             glm::mat4(1.0f),
+                             glm::mat4(1.0f),
+                             glm::mat4(1.0f),
+                             glm::mat4(1.0f),
+                             glm::mat4(1.0f),
+                             glm::mat4(1.0f),
+                             glm::mat4(1.0f),
+                             glm::mat4(1.0f),
+                             glm::mat4(1.0f)};
 
+glm::mat4 view            =  glm::mat4(1.0f);
+glm::mat4 projection      =  glm::mat4(1.0f);
+//------------------------------------------------------------------------------
 
+//------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+//------------------------------------------------------------------------------
+void leftButtonPressedRotation();
+//------------------------------------------------------------------------------
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+//------------------------------------------------------------------------------
 void processInput(GLFWwindow *window);
+//------------------------------------------------------------------------------
 GLFWwindow* windowInit();
-void windowLoop(GLFWwindow* window, float** vertices, int nVertices, int nClouds, int buffersize, cloudShader shader) ;
+//------------------------------------------------------------------------------
+void windowLoop(GLFWwindow* window, float** vertices, int nVertices, int nClouds, int buffersize, cloudShader shader);
+//------------------------------------------------------------------------------
 void drawThread(float **vertices, int nVertices, int nClouds, int buffersize);
+//------------------------------------------------------------------------------
 
 #endif
